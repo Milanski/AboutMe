7e08e9a5e5844ce345e45685e8de3856962f8a0e
Milancho Panovski <milancho.panovski@de.bosch.com>
2025-03-03 16:34:52 +0100

    change text

--- AboutMe/docusaurus.config.js
+++ AboutMe/docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Milancho Panovski',
-  tagline: 'Distinguished Software Architect and Developer',
+  tagline: 'Principle Software Architect and Developer / Tech Lead',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
